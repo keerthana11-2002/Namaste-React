ddc6bf5f92936b3b90cd0f3c4835b4e2b658da7d
kabil1001 <jkapil515@gmail.com>
2024-06-18 21:27:03 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,13 @@
+const result = React.createElement("div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "i'm in h1 tag"),
+        React.createElement("h2", {}, "i'm in h2 tag")
+    ]),
+    React.createElement("div", { id: "child2" },[
+            React.createElement("h1", {}, "i'm in h1 tag"),
+            React.createElement("h2", {}, "i'm in h2 tag")
+        ])
+    )
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(result)      
---
+++ index.js
@@ -0,0 +1,5 @@
+
+// const msg = React.createElement("h1",{},"I am creating the Hello World in the React")
+// 
+
+
